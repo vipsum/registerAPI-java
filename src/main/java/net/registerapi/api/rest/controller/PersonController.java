a8d7e94d8c5e95183d87eb16eb22dd0aa2529548
vipsum <83631487+vipsum@users.noreply.github.com>
2022-07-08 03:14:44 -0300

    added all functionalities

--- src/main/java/net/registerapi/api/rest/controller/PersonController.java
+++ src/main/java/net/registerapi/api/rest/controller/PersonController.java
@@ -1,13 +1,10 @@
 package net.registerapi.api.rest.controller;
 
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
