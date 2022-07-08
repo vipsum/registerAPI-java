8f2a2431f3e309b2dee2fe89d038354cd5ec5d54
vipsum <83631487+vipsum@users.noreply.github.com>
2022-07-08 03:06:19 -0300

    added all functionalities

--- src/main/java/net/registerapi/api/rest/controller/PersonController.java
+++ src/main/java/net/registerapi/api/rest/controller/PersonController.java
@@ -8,11 +8,9 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +28,25 @@ public class PersonController {
         return personRepository.save(person);
     }
 
-    @PostMapping
+    @PostMapping("/update") //update gender
+    public Person updatePerson(@RequestBody Person person){
+        Person personInDb = personRepository.findByDni(person.getDni());
+        
+        person.setId(personInDb.getId());
+        person.setName(personInDb.getName());
+        person.setSurname(personInDb.getSurname());
+        person.setBirthdate(personInDb.getBirthdate());
+
+        return personRepository.save(person);
+    }
+
+    @GetMapping("/findByDni/{dni}")
+    public Person findByDni(@PathVariable int dni){
+        return personRepository.findByDni(dni);
+    }
+
+    @GetMapping("/findLikeSurname/{surname}")
+    public List<Person> findLikeSurname(@PathVariable String surname){
+        return personRepository.searchBySurnameStartsWithIgnoreCase(surname);
+    }
 }
--- src/main/java/net/registerapi/api/rest/repository/PersonRepository.java
+++ src/main/java/net/registerapi/api/rest/repository/PersonRepository.java
@@ -1,9 +1,12 @@
 package net.registerapi.api.rest.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import net.registerapi.api.rest.model.Person;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long>{
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Person findByDni(int dni);
+    List<Person> searchBySurnameStartsWithIgnoreCase(String surname);
 }
